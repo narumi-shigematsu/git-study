5ddb36b864d1a4a2a51b6c0858f3bda373e7b83b
Narumi Shigematsu <magpie759mic@outlook.jp>
2022-08-02 14:35:46 +0900

    week5 study

---
+++ sample.js
@@ -0,0 +1,20 @@
+// const a=2;
+// const b=3;
+// const sum = a+b;
+
+// console.log(sum);
+for (let i=0; i<5 ;i++){
+    console.log(i);
+}
+
+const price = 50;
+if(price >500){
+    console.log("mo");
+}else if(price >100){
+    console.log("neko");
+}else{
+    console.log("po");
+}
+
+const array = ["a", "b", "c", "d", "e", "f"];
+console.log(array[0]);
